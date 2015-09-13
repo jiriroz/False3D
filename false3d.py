410a93f417d00cb4ada5096484e472cf98785449
jirkaroz <jiri.roznovjak@gmail.com>
2015-09-13 13:22:26 -0400

    Updated class, improved functionality

--- false3d.py
+++ false3d.py
@@ -1,12 +1,10 @@
 import cv2
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-
+from collections import deque
 
 def main():
-    
-    eyeData = EyeData()
-
+    tracking = EyeFaceTracking()
     cap = cv2.VideoCapture(1)
     frameCount = 0
 
@@ -14,23 +12,18 @@ def main():
         frame, gray = nextFrame(cap)
         frameCount += 1
 
-        if eyeData.tracking:
-            track("haar", gray, eyeData)
-        if not eyeData.tracking:
-            detectEyes(gray, eyeData)
+        if tracking.tracking:
+            tracking.track(gray)
+        if not tracking.tracking:
+            tracking.detect(gray)
 
-        displayState(eyeData.tracking)
-        displayFrame(frame, eyeData.eyes)
-
-        if frameCount > 100:
+        displayFrame(frame, tracking)
+        if frameCount > 200:
             break
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    print "detection rate", eyeData.detectionRate()
-    print "track rate", eyeData.redetectionRate()
-
+    tracking.reportStats()
     cap.release()
     cv2.destroyAllWindows()
 
@@ -46,158 +39,140 @@ def nextFrame(cap):
 """
 Display frame and everything it is supposed to display.
 """
-def displayFrame(frame, eyes):
-    displayEyes(frame, eyes)
+def displayFrame(frame, tracking):
+    displayFeatures(frame, tracking)
     cv2.imshow("Video", frame)
 
-def displayEyes(frame, eyes):
-    for (ex,ey,ew,eh) in eyes:
+def displayFeatures(frame, tracking):
+    for (ex,ey,ew,eh) in tracking.eyes:
         cv2.rectangle(frame, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+    (fx,fy,fw,fh) = tracking.face
+    cv2.rectangle(frame, (fx,fy), (fx+fw,fy+fh), (255,0,0), 2)
 
 """
-Displays state of the program to the command line.
+Class performing all feature detection and tracking.
 """
-def displayState(tracking):
-    return
-    if tracking:
-        print "Eye detection succesful"
-    else:
-        print "Eye detection not succesful"
-
-"""
-Detect eyes using haar cascade classifier.
-First detect face and within it search for eyes.
-Return positive iff len(faces) == 1 and len(eyes) == 2.
-"""
-def detectEyes(gray, eyeData):
-    eyeData.attemptedDetections += 1
-    #print "attempting to detect eyes"
-    faces = eyeData.faceCascade.detectMultiScale(gray, 1.3, 5)
-    if len(faces) != 1:
-        eyeData.tracking = False
-        return
-    (x,y,w,h) = faces[0]
-    roiGray = gray[y:y+h, x:x+w]
-    eyes = eyeData.eyeCascade.detectMultiScale(roiGray)
-    if len(eyes) != 2:
-        eyeData.tracking = False
-        return
-    eyes = map(lambda (ex,ey,ew,eh):(ex+x,ey+y,ew,eh), eyes)
-    keypts = []
-    descr = []
-    for (ex,ey,ew,eh) in eyes:
-        roiEye = gray[ey:ey+eh,ex:ex+ew]
-        localPts, localDes = eyeData.orb.detectAndCompute(roiEye, None)
-        keypts.append(localPts)
-        descr.append(localDes)
-    #print keypts
-    eyeData.eyes = eyes
-    eyeData.keypts = keypts
-    eyeData.descr = descr
-    eyeData.tracking = True
-    eyeData.successfulDetections += 1
-
-"""
-Attempt eye tracking according to the selected method.
-"""
-def track(mode, gray, eyeData):
-    assert mode in ["haar", "keypoints"], "Tracking mode doesn't exist."
-    eyeData.attemptedRedetections += 1
-    if mode == "haar":
-        trackEyesHaar(gray, eyeData)
-    elif mode == "keypoints":
-        trackEyesKeypt(gray, eyeData)
-    if eyeData.tracking:
-        eyeData.successfulRedetections += 1
-
-"""
-Detect eyes in a small region around the previous position using
-haar cascades.
-"""
-def trackEyesHaar(gray, eyeData):
-    newEyes = []
-    horiz = 10
-    vert = horiz / 2
-    for (ex,ey,ew,eh) in eyeData.eyes:
-        x, y = ex-horiz, ey-vert
-        roiGray = gray[x:ex+ew+horiz, y:ey+eh+vert]
-        eye = eyeData.eyeCascade.detectMultiScale(roiGray)
-        if len(eye) != 1:
-            eyeData.tracking = False
-            return
-        eye = eye[0]
-        eye = (eye[0] + x, eye[1] + y, eye[2], eye[3])
-        newEyes.append(eye)
-    eyeData.tracking = True
-    eyeData.eyes = newEyes
-
-"""
-Detect eyes in a small region around the previous position
-using keypoint descriptor.
-"""
-def trackEyesKeypt(gray, eyeData):
-    #print "attempting to track eyes"
-    newEyes = []
-    horiz = 10
-    vert = horiz / 2
-    avgdx = 0
-    avgdy = 0
-    count = 0
-    for i in range(len(eyeData.eyes)):
-        (ex,ey,ew,eh) = eyeData.eyes[i]
-        x, y = ex-horiz, ey-vert
-        roiGray = gray[x:ex+ew+horiz, y:ey+eh+vert]
-        newKp, newDes = eyeData.orb.detectAndCompute(roiGray, None)
-        if len(newKp) == 0:
-            eyeData.tracking = False
-            return
-        matches = eyeData.matcher.match(eyeData.descr[i], newDes)
-        matches = sorted(matches, key = lambda x:x.distance)
-        matches = matches[:5]
-        for match in matches:
-            count += 1
-            oldPt = eyeData.keypts[i][match.trainIdx]
-            newPt = newKp[match.queryIdx]
-            dx = oldPt[0] - newPt[0]
-            dy = oldPt[1] - newPt[1]
-            avgdx += dx
-            avgdy += dy
-    #avgdx /= count
-    #avgdy /= count
-    eyeData.tracking = True
-    eyeData.eyes = newEyes
-
-"""
-Class holding all data needed for eye detection and tracking,
-independent of the method.
-"""
-class EyeData:
+class EyeFaceTracking:
     def __init__(self):
-        facecas = "haarcascade_frontalface_default.xml"
-        eyecas = "haarcascade_eye.xml"
+        classifDir = "classifiers/"
+        facecas = classifDir + "haarcascade_frontalface_default.xml"
+        eyecas = classifDir + "haarcascade_eye.xml"
         self.faceCascade = cv2.CascadeClassifier(facecas)
         self.eyeCascade = cv2.CascadeClassifier(eyecas)
         self.orb = cv2.ORB()
         self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
         self.tracking = False
-        self.eyes = []
-        self.keypts = None
-        self.descr = None
-        self.attemptedDetections = 0
-        self.successfulDetections = 0
-        self.attemptedRedetections = 0
-        self.successfulRedetections = 0
+        self.eyes = [(0,0,0,0), (0,0,0,0)]
+        self.face = (0,0,0,0)
+        self.smoothN = 2
+        self.faceHistory = deque()
+        self.eyeHistory = deque()
+        self.setupStats()
 
-    def redetectionRate(self):
-        return float(self.successfulRedetections)/self.attemptedRedetections
+    def setupStats(self):
+        self.attemptedDetect = 0
+        self.successfulDetect = 0
+        self.attemptedRedetect = 0
+        self.successfulRedetect = 0
+        self.attemptedFaceDetect = 0
+        self.successfulFaceDetect = 0
+        self.attemptedFaceRedetect = 0
+        self.successfulFaceRedetect = 0
 
-    def detectionRate(self):
-        return float(self.successfulDetections)/self.attemptedDetections
+    """
+    Detect eyes using haar cascade classifier.
+    First detect face and within it search for eyes.
+    """
+    def detect(self, gray):
+        self.attemptedDetect += 1
+        self.attemptedFaceDetect += 1
+        ret, faces = self.searchForFaces(gray)
+        if not ret:
+            self.tracking = False
+            return
+        self.successfulFaceDetect += 1
+        (fx,fy,fw,fh) = faces[0]
+        self.face = faces[0]
+        self.updateFace(faces[0])
+        ret, eyes = self.searchForEyes(gray[fy:fy+fh, fx:fx+fw])
+        if not ret:
+            self.tracking = False
+            return
+        eyes = map(lambda (ex,ey,ew,eh):(ex+fx,ey+fy,ew,eh), eyes)
+        self.eyes = eyes
+        self.tracking = True
+        self.successfulDetect += 1
 
+    """
+    Detect eyes in a small region around the previous position using
+    haar cascades.
+    """
+    def track(self, gray):
+        newEyes = []
+        (fx, fy, fw, fh) = self.face
+        #search for the face in an area around the previous face
+        xMargin = fw // 3
+        yMargin = fh // 4
+        roiFace = gray[fy-yMargin:fy+fh+yMargin, fx-xMargin:fx+fw+xMargin]
+        ret, faces = self.searchForFaces(roiFace)
+        self.attemptedFaceRedetect += 1
+        if not ret:
+            self.tracking = False
+            return
+        self.successfulFaceRedetect += 1
+        (newfx,newfy,fw,fh) = faces[0]
+        fx, fy = newfx+fx-xMargin, newfy+fy-yMargin
+        self.face = (fx, fy, fw, fh)
+        ret, eyes = self.searchForEyes(gray[fy:fy+fh, fx:fx+fw])
+        self.attemptedRedetect += 1
+        if not ret:
+            self.tracking = False
+            return
+        self.successfulRedetect += 1
+        eyes = map(lambda (ex,ey,ew,eh):(ex+fx,ey+fy,ew,eh), eyes)
+        self.eyes = eyes
 
+    def searchForFaces(self, roi, scaleF=1.3, minNeighbors=5):
+        faces = self.faceCascade.detectMultiScale(roi, scaleF, minNeighbors)
+        return len(faces) == 1, faces
+
+    def searchForEyes(self, roi, scaleF=1.3, minNeighbors=5):
+        eyes = self.eyeCascade.detectMultiScale(roi, scaleF, minNeighbors)
+        return len(eyes) == 2, eyes
+
+    def updateFace(self, face):
+        self.faceHistory.append(face)
+        #keep most recent face for fast access
+        self.face = face
+        if len(self.faceHistory) > self.smoothN:
+            self.faceHistory.popleft()
+
+    def reportStats(self):
+        eyeDetect = float(self.successfulDetect)/self.attemptedDetect
+        eyeRedetect = float(self.successfulRedetect)/(self.attemptedRedetect + 0.01)
+        faceDetect = float(self.successfulFaceDetect)/self.attemptedFaceDetect
+        faceRedetect = float(self.successfulFaceRedetect)/(self.attemptedFaceRedetect + 0.01)
+        print "face detections: " + str(faceDetect)
+        print "face redetections: " + str(faceRedetect)
+        print "eye detections: " + str(eyeDetect)
+        print "eye redetections: " + str(eyeRedetect)
+
+    def getSmoothedFace(self):
+        if len(self.faceHistory) == 0:
+            return self.face
+        return self.average(list(self.faceHistory))
+
+    def getSmoothedEyes(self):
+        return
+
+    """Takes a list of tuples and returns their average"""
+    def average(self, tuples):
+        array = np.array(tuples, dtype=np.float64)
+        mean = np.mean(array, 0).astype(np.int32)
+        return tuple(mean)
+            
 
 
 if __name__ == "__main__":
     main()
-
 
