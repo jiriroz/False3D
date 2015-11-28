4b6c682af79291f453d980ec632540a91e8d3153
jirkaroz <jiri.roznovjak@gmail.com>
2015-11-28 03:52:14 -0500

    Tuning eye tracking

--- false3d.py
+++ false3d.py
@@ -1,11 +1,10 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import deque
 
 class False3D:
     def run(self, camera):
-        self.tracker = EyeFaceTracker(eyeMode = True, smoothMode = False)
+        self.tracker = EyeFaceTracker(eyeMode = True)
         self.displayer = ObjectDisplayer()
         cap = cv2.VideoCapture(camera)
         frameCount = 0
@@ -45,9 +44,13 @@ class False3D:
     
     def displayFeatures(self, frame):
         (fx,fy,fw,fh) = self.tracker.facePosition
-        if self.tracker.smoothMode:
-            (fx,fy,fw,fh) = self.tracker.getSmoothedFace()
         cv2.rectangle(frame, (fx,fy), (fx+fw,fy+fh), (255,0,0), 2)
+        #######
+        topMargin = int(self.tracker.topMargin * fh)
+        bottomMargin = int((1-self.tracker.bottomMargin) * fh)
+        cv2.rectangle(frame, (fx,fy), (fx+fw, fy+topMargin), (255,0,0), -1)
+        cv2.rectangle(frame, (fx,fy+bottomMargin), (fx+fw, fy+fh), (255,0,0), -1)
+        #######
         if self.tracker.eyeMode:
             for (ex,ey,ew,eh) in self.tracker.eyePositions:
                 cv2.rectangle(frame, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
@@ -62,10 +65,9 @@ class EyeFaceTracker:
     property variables.
     @param eyeMode boolean determining whether we should track eyes
     """
-    def __init__(self, eyeMode=False, smoothMode=False):
+    def __init__(self, eyeMode=False):
         #denotes whether we should track eyes or not
         self.eyeMode = eyeMode
-        self.smoothMode = smoothMode
         classifDir = "classifiers/"
         facecas = classifDir + "haarcascade_frontalface_default.xml"
         eyecas = classifDir + "haarcascade_eye.xml"
@@ -78,14 +80,18 @@ class EyeFaceTracker:
         self.eyePositions = [(0,0,0,0), (0,0,0,0)]
         self.facePosition = (0,0,0,0)
         self.perspective = (0,0)
+        #Portion of face from the top to exclude
+        self.topMargin = 0.2
+        #Portion of face from the bottom to exclude
+        self.bottomMargin = 0.3
         self.face = None
         self.eyes = [None, None]
-        self.smoothN = 2
+        #above this threshold any shift in eyes or face is rejected
+        #it is two standard deviations above the observed mean
+        self.shiftThreshold = 10
         #termination criteria for meanshift
         self.maxIterations = 10
         self.termCrit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, self.maxIterations, 1)
-        self.facePositionHistory = deque()
-        self.eyeHistory = deque()
 
     """Search for and track face and eyes."""
     def track(self, frameBGR, gray):
@@ -111,14 +117,14 @@ class EyeFaceTracker:
         (fx,fy,fw,fh) = self.facePosition
         ret, eyes = self.searchForEyes(gray[fy:fy+fh, fx:fx+fw])
         if not ret:
-            dx, dy = self.xShift, self.yShift
+            dx, dy = self.xFaceShift, self.yFaceShift
             eyes = self.eyePositions
             eyes = map(lambda (ex,ey,ew,eh):(ex+dx,ey+dy,ew,eh), eyes)
-            self.eyePositions = eyes
+            self.updateEyes(eyes)
             return False
 
         eyes = map(lambda (ex,ey,ew,eh):(ex+fx,ey+fy,ew,eh), eyes)
-        self.eyePositions = eyes
+        self.updateEyes(eyes)
         return True
         
     """Track face using meanshift."""
@@ -137,7 +143,6 @@ class EyeFaceTracker:
         ret, newFacePos = cv2.meanShift(backProj, facePos, self.termCrit)
 
         if ret == self.maxIterations:
-            self.updateFace((0,0,0,0))
             return False
         self.updateFace(newFacePos)
         return True
@@ -147,21 +152,31 @@ class EyeFaceTracker:
         (fx,fy,fw,fh) = self.facePosition
         ret, eyes = self.searchForEyes(gray[fy:fy+fh, fx:fx+fw])
         if not ret:
-            dx, dy = self.xShift, self.yShift
+            dx, dy = self.xFaceShift, self.yFaceShift
             eyes = self.eyePositions
             eyes = map(lambda (ex,ey,ew,eh):(ex+dx,ey+dy,ew,eh), eyes)
-            self.eyePositions = eyes
+            self.updateEyes(eyes)
             return False
         eyes = map(lambda (ex,ey,ew,eh):(ex+fx,ey+fy,ew,eh), eyes)
-        self.eyePositions = eyes
+        self.updateEyes(eyes)
         return True
 
     def searchForFaces(self, roi, scaleF=1.3, minNeighbors=5):
         faces = self.facePositionCascade.detectMultiScale(roi, scaleF, minNeighbors)
         return len(faces) == 1, faces
 
-    def searchForEyes(self, roi, scaleF=1.3, minNeighbors=5):
-        eyes = self.eyeCascade.detectMultiScale(roi, scaleF, minNeighbors)
+    """
+    Search for eyes using viola jones within the face.
+    Exclude an upper and lower portion of the face.
+    """
+    def searchForEyes(self, face, scaleF=1.3, minNeighbors=5):
+        height = face.shape[0]
+        top = height * self.topMargin
+        bottom = height * (1 - self.bottomMargin)
+        faceRestricted = face[:bottom, :]
+        faceRestricted = faceRestricted[top:, :]
+        eyes = self.eyeCascade.detectMultiScale(faceRestricted, scaleF, minNeighbors)
+        eyes = map(lambda (ex,ey,ew,eh):(int(ex),int(ey+top),int(ew),int(eh)), eyes)
         if len(eyes) != 2:
             return False, None
         return self.validateEyes(eyes), eyes
@@ -185,48 +200,40 @@ class EyeFaceTracker:
                 self.eyes[i] = frame[ey:ey+eh, ex:ex+ew]
 
     """
-    Store the current face. Also, as an experimental feature, keep a queue
-    of faces to smooth the tracking rectangle movement.
+    Store the current face.
     """
     def updateFace(self, face):
-        self.facePositionHistory.append(face)
-        self.xShift = face[0] - self.facePosition[0]
-        self.yShift = face[1] - self.facePosition[1]
-        self.facePosition = face
-        #make space for next face
-        if len(self.facePositionHistory) > self.smoothN:
-            self.facePositionHistory.popleft()
+        self.xFaceShift = face[0] - self.facePosition[0]
+        self.yFaceShift = face[1] - self.facePosition[1]
+        valid1 = abs(self.xFaceShift) < self.shiftThreshold
+        valid2 = abs(self.yFaceShift) < self.shiftThreshold
+        #Apply the threshold iff we are tracking face
+        valid = not self.isTrackingFace or (valid1 and valid2)
+        self.trackingFace = valid
+        if True:
+            self.facePosition = face
+
+    def updateEyes(self, eyes):
+        dx1 = (eyes[0][0] - self.eyePositions[0][0])
+        dx2 = (eyes[1][0] - self.eyePositions[1][0])
+        dy1 = (eyes[0][1] - self.eyePositions[0][1])
+        dy2 = (eyes[1][1] - self.eyePositions[1][1])
+        thres = self.shiftThreshold
+        valid = all([abs(dx1), abs(dx2), abs(dy1), abs(dy2)]) < thres
+        #Apply the threshold iff we are tracking eyes
+        valid = valid or not self.isTrackingEyes
+        if True:
+            self.eyePositions = eyes
 
     """
     Compute the approximate point of perspective.
     It will be exactly between the two eyes.
     """
     def computePerspective(self):
-        #(fx,fy,fw,fh) = self.facePosition
-        #self.perspective = (fx + fw/2, fy + fh/2)
-
         eye1, eye2 = self.eyePositions[0], self.eyePositions[1]
         x1, y1 = eye1[0] + eye1[2]/2, eye1[1] + eye1[3]/2
         x2, y2 = eye2[0] + eye2[2]/2, eye2[1] + eye2[3]/2
         self.perspective = (x1 + (x2-x1)/2, y1 + (y2-y1)/2)
-
-    """
-    Get the average of several previous posisions of the face
-    to make the shift look smooth.
-    """
-    def getSmoothedFace(self):
-        if len(self.facePositionHistory) == 0:
-            return self.facePosition
-        return self.average(list(self.facePositionHistory))
-
-    def getSmoothedEyes(self):
-        return
-
-    """Takes a list of tuples and returns their average"""
-    def average(self, tuples):
-        array = np.array(tuples, dtype=np.float64)
-        mean = np.mean(array, 0).astype(np.int32)
-        return tuple(mean)
 
 """
 This class will display and rotate 3D projection of an image
